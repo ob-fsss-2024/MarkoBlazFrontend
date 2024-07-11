af53c0adc57186d95496b5f6cc43713bbe617be0
Marko Lumbar <markolumbar@Markos-MacBook-Pro.local>
2024-07-11 14:31:37 +0200

    simple branje apija

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private httpClient:HttpClient){}
+
+  drinks: any;
+
+  ngOnInit() {
+    this.httpClient.get("http://localhost:9090/coctails?coctailName=Mojito").subscribe(data => this.drinks = data);
+    
+  }
   title = 'aplication';
 }
