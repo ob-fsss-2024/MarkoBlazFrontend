f2b9079d7d9bb14630ce1720ed535dcec51fded9
Marko Lumbar <markolumbar@Markos-MacBook-Pro.local>
2024-07-11 15:52:11 +0200

    Dela tudi input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,35 @@
+// app.component.ts
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 import { CocktailService } from './services/cocktail.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, CommonModule],
+  imports: [RouterOutlet, HttpClientModule, CommonModule, FormsModule], // Add FormsModule to imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
   providers: [CocktailService]
-  
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  constructor(private myService:CocktailService){}
+  constructor(private myService: CocktailService) { }
 
   drinks: any;
+  searchQuery: string = '';
 
   ngOnInit() {
     this.myService.getCocktail("Mojito").subscribe(data => this.drinks = data);
-    
   }
-  title = 'aplication';
+
+  searchCocktail() {
+    if (this.searchQuery.trim()) {
+      this.myService.getCocktail(this.searchQuery).subscribe(data => this.drinks = data);
+    }
+  }
+
+  title = 'application';
 }
